e187750479861c0f40a471bc4ee0b14fe6cfa99f
Morlis <marco.morais@gmail.com>
2019-06-29 20:06:32 +0100

    Example maven project

---
+++ src/main/java/com/marcomorais/datastructures/App.java
@@ -0,0 +1,13 @@
+package com.marcomorais.datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
